9cf5413a0a7fca8d3099677b342de8ceacc2fb98
By4r <j.alfarno142@gmail.com>
2024-01-24 10:04:47 +0300

    Stack Signals

---
+++ Assets/Scripts/Runtime/Signals/StackSignals.cs
@@ -0,0 +1,17 @@
+﻿using strange.extensions.signal.impl;
+using UnityEngine;
+
+namespace Runtime.Signals
+{
+    public class StackSignals
+    {
+        public Signal<GameObject> onInteractionATM = new Signal<GameObject>();
+        public Signal<GameObject> onInteractionObstacle = new Signal<GameObject>();
+        public Signal<GameObject> onInteractionCollectable = new Signal<GameObject>();
+        public Signal onInteractionObstacleWithPlayer = new Signal();
+        public Signal<Vector2> onStackFollowPlayer = new Signal<Vector2>();
+        public Signal onUpdateType = new Signal();
+        public Signal onInteractionConveyor = new Signal();
+        public Signal onUpdateAnimation = new Signal();
+    }
+}
